ae4a5f30ef045d9e169ead2709cb4f09248a1e8d
Emil Weigelt Kjær Nielsen <emil601b@edu.campusvejle.dk>
2019-06-21 09:06:36 +0200

    Add project files.

---
+++ Warrior Battle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warrior_Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior maximus = new Warrior
+                ("Maximus", 1000, 120, 40);
+
+            Warrior bob = new Warrior
+                ("Bob", 1000, 120, 40);
+
+            Battle.StartFight(maximus, bob);
+
+            Console.ReadLine();
+        }
+    }
+}
